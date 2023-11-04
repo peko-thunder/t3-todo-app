18f117c58168b825873b4940895b990f67cb3679
OyamaP <eag000gle.0.3151993@gmail.com>
2023-11-04 16:47:05 +0900

    init

---
+++ src/schema/todo.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const createTaskSchema = z.object({
+  title: z.string().max(20),
+  body: z.string().min(5),
+});
+
+export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
+
+export const updateTaskSchema = z.object({
+  taskId: z.string().cuid(),
+  title: z.string().max(20),
+  body: z.string().min(5),
+});
+
+export type UpdateTaskInput = z.TypeOf<typeof updateTaskSchema>;
